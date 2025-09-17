4d1afd4544c201f76ce8b8f2ee7cd12f1cc559cb
jeaglecode <32748770+jeaglecode@users.noreply.github.com>
2025-09-17 15:23:37 -0700

    Initial Commit

---
+++ FrostPTO/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FrostPTO
+//
+//  Created by Jesse Duenas on 9/17/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FrostPTO/FrostPTOApp.swift
@@ -0,0 +1,17 @@
+//
+//  FrostPTOApp.swift
+//  FrostPTO
+//
+//  Created by Jesse Duenas on 9/17/25.
+//
+
+import SwiftUI
+
+@main
+struct FrostPTOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
